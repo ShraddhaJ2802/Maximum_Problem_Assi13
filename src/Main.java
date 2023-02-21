a8b874794e589cae15d568d7b775f32da614df46
Shraddha <jadhavshradddha5@gmail.com>
2023-02-21 07:49:24 +0530

    This is second uc

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import com.bridgelab.maximum.MaximumProblem;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome In generic Problem");
 
-       /*  MaximumProblem max=new MaximumProblem();
+        MaximumProblem max=new MaximumProblem();
         Integer x = new Integer("45");
         Integer y= new Integer("58");
         Integer z = new Integer("15");
 
-        System.out.printf("Maximum of %d, %d and %d is %d\n\n", x, y,z , max.getMaxInteger(x, y, z));*/
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", x, y,z , max.getMaxInteger(x, y, z));
     }
+
 }
--- src/com/bridgelab/maximum/MaximumProblem.java
+++ src/com/bridgelab/maximum/MaximumProblem.java
@@ -1,8 +1,19 @@
 package com.bridgelab.maximum;
 
 public class MaximumProblem {
-    public Integer getMaxInteger(Integer num1 , Integer num2 , Integer num3) {
+  /*  public Integer getMaxInteger(Integer num1 , Integer num2 , Integer num3) {
         Integer max = num1; // assume x is initially the largest
+
+        if (num2.compareTo(max) > 0)
+            max = num2; // num2 is the largest so far
+
+        if (num3.compareTo(max) > 0)
+            max = num3; // num3 is the largest
+
+        return max; // returns the largest object
+    }*/
+    public Float getMaxFloat(Float num1 , Float num2 , Float num3) {
+        Float max = num1; // assume x is initially the largest
 
         if (num2.compareTo(max) > 0)
             max = num2; // num2 is the largest so far
