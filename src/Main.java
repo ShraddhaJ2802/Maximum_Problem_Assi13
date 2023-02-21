d51ed9af8ec62830bdbe793c0bf1600113a3d457
Shraddha <jadhavshradddha5@gmail.com>
2023-02-21 06:38:17 +0530

    This is assignment 13

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome In generic Problem");
+
+       /*  MaximumProblem max=new MaximumProblem();
+        Integer x = new Integer("45");
+        Integer y= new Integer("58");
+        Integer z = new Integer("15");
+
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", x, y,z , max.getMaxInteger(x, y, z));*/
+    }
+}
